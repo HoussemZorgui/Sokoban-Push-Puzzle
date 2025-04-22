b12c3c043e6ae76eaa2bd4e776875c2687b00cee
Houssem Zorgui <houssemzorgui10@gmail.com>
2025-04-22 14:45:51 +0100

    Prepare for GitHub Pages deployment

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'main_game_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    // Force Canvas renderer for better Flame compatibility
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+
   runApp(const App());
 }
 
@@ -13,7 +20,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'PushPuzzle',
+      title: 'Sokoban Push Puzzle - Houssem Zorgui',
       home: MainGamePage(),
     );
   }
